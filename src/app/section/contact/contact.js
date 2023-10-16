6753a1970f2e06844d480c09632fdec170727f77
MT1607 <=>
2023-10-16 08:51:26 +0700

    remove-information-contact

--- src/app/section/contact/contact.js
+++ src/app/section/contact/contact.js
@@ -9,12 +9,6 @@ const Contact = () => {
         <div className="container">
           <div className="section_title">
             <h2>Contact</h2>
-            <p>
-              Magnam dolores commodi suscipit. Necessitatibus eius consequatur
-              ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam
-              quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea.
-              Quia fugiat sit in iste officiis commodi quidem hic quas.
-            </p>
           </div>
 
           <div className="row" data-aos="fade-in">
